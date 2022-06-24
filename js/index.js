d44f693254a35e431a59437b1e2cb93c5d2630e0
José Nascimento <vittorr.nascimento@gmail.com>
2022-06-24 16:21:43 -0300

    Project-fw II

--- js/index.js
+++ js/index.js
@@ -5,4 +5,21 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 
-}
+};
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});

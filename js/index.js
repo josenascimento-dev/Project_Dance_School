f1f6e28dc578fc29a24fba815ef7bc6e02cde660
José Nascimento <vittorr.nascimento@gmail.com>
2022-06-27 16:52:12 -0300

    Project-fw IV

--- js/index.js
+++ js/index.js
@@ -23,3 +23,22 @@ var swiper = new Swiper(".home-slider", {
       clickable: true,
     },
 });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnIntereaction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        600:{
+            slidesPerview:2,
+        }
+    }
+});
